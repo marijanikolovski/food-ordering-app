714a75475441d1146c833775cf25e1b448c4f000
marijanikolovski <m.kuzeljevic@gmail.com>
2023-03-29 18:31:09 +0200

    Cookie Auth

---
+++ src/components/Add.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+      New pizza
+    </div>
+  )
+}
+
+export default Add
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,15 @@ import { Inter } from 'next/font/google'
 import { Featured } from '@/components/Featured'
 import { PizzaList } from '@/components/PizzaList'
 import axios from 'axios'
+import { useState } from 'react'
+import Add from '@/components/Add'
+import AddButton from '@/components/AddButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ pizzaList }) {
+export default function Home({ pizzaList, admin }) {
+  const [close, setClose] = useState(true);
+
   return (
     <>
       <Head>
@@ -17,16 +22,26 @@ export default function Home({ pizzaList }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <PizzaList pizzaList={pizzaList} />
+      {!close && <Add setClose={setClose} />}
     </>
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
   const res = await axios.get('http://localhost:3000/api/products');
   return {
     props: {
-      pizzaList: res.data
+      pizzaList: res.data,
+      admin
     }
   }
 }
